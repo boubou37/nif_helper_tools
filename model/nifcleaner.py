3d7345304f64b7581e6e904ff25c97442474b195
lrhmf <lrahamefy@sfr.fr>
2017-11-07 23:54:36 +0100

    added handling of skeleton.nif + additionnal comments

--- model/nifcleaner.py
+++ model/nifcleaner.py
@@ -11,11 +11,13 @@ class NifConverter:
         self.files = []
 
     def nif_to_blender(self):
+        # lookup by folder name
         if self.pathname != '' and os.path.isdir(self.pathname):
             for astream, adata in NifFormat.walkData(self.pathname):
                 retcode = self.__do_convert_nif_to_blender(astream, adata)
                 if retcode > 0:
                     break
+        # lookup by file names
         elif self.files:
             for file in self.files:
                 stream = open(file,'rb')
@@ -31,6 +33,20 @@ class NifConverter:
 
         data.read(stream)
         for root in data.roots:
+            # special handling of skeleton.nif
+            # could not use list comprehension due to AttributeError and the fact we can't set the extra_data_list attr
+            size = root.num_extra_data_list
+            if size > 0:
+                size = root.num_extra_data_list
+                if size > 0:
+                    index = 0
+                    for i in range(size):
+                        if type(root.extra_data_list[i]) == NifFormat.BSBoneLODExtraData:
+                            index = i
+                            break
+                    root.extra_data_list[index] = None
+
+            # removal of BSLightingShaderProperty nodes
             for block in root.tree():
                 if type(block) == NifFormat.NiTriShape:
                     if type(block.bs_properties[0]) == NifFormat.BSLightingShaderProperty:
@@ -38,7 +54,7 @@ class NifConverter:
                     if block.bs_properties[1] is not None:
                         block.bs_properties[1] = None
 
-
+        # we append '_blender' to the file name
         namenoext = os.path.basename(os.path.splitext(stream.name)[0])
         stream = open(stream.name.replace(namenoext, namenoext + '_blender'), 'wb')
 
--- test_read.py
+++ test_read.py
@@ -1,7 +1,7 @@
 from pyffi.formats.nif import *
 import os
-stream = open('snippets/initTextures.py','rb')
-
-nameNoExt = os.path.basename(os.path.splitext(stream.name)[0])
-print(stream.name.replace(nameNoExt,nameNoExt+'_blender'))
-print(os.path.basename(stream.name))
+stream = open('skeleton.nif','rb')
+data = NifFormat.Data()
+data.read(stream)
+for root in data.roots:
+    print(root.extra_data_list)
